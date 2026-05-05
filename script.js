fd159432aa2648ddb914888e0258374bca883aa0
Yang Liu <31658700+liuyang151617@users.noreply.github.com>
2026-05-05 11:38:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const items = document.querySelectorAll('.item');
+
 const buttons = document.querySelectorAll('.filter-bar button');
+const items = document.querySelectorAll('.item');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
+    document.querySelector('.active').classList.remove('active');
+    btn.classList.add('active');
+
     const filter = btn.dataset.filter;
 
     items.forEach(item => {
       item.style.display =
         filter === 'all' || item.classList.contains(filter)
-        ? 'block'
-        : 'none';
+          ? 'block'
+          : 'none';
     });
   });
 });
 
-// lightbox
+// Lightbox
 const images = document.querySelectorAll('.item img');
 const lightbox = document.getElementById('lightbox');
 const lightboxImg = document.getElementById('lightbox-img');
